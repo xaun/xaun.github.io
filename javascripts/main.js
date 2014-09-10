4112c805cfbb78015bee9c90990aab11726830ca
Xaun Lopez <xaunlopez@gmail.com>
2014-09-10 15:01:35 +1000

    nav, about, 1 project

---
+++ javascripts/main.js
@@ -0,0 +1,8 @@
+(function() {
+  $(document).ready(function() {
+    var windowHeight;
+    windowHeight = $(window).height();
+    return $('#about').css('height', windowHeight);
+  });
+
+}).call(this);
